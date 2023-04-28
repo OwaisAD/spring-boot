cf648c89d041f920dafbbb795bc109946d1fb5ad
Owais Dashti <owaisdashti@outlook.com>
2023-04-28 22:06:30 +0200

    setting up a get endpoint

--- src/main/java/com/example/demo/owais/Main.java
+++ src/main/java/com/example/demo/owais/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -13,7 +15,12 @@ public class Main {
     }
 
     @GetMapping("/")
-    public String greet() {
-        return "Hello World!";
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse("Hello World!", List.of("Java", "TypeScript"), new Person("Elon", 24, 1_000, true));
+        return response;
     }
+
+    record Person(String name, int age, double savings, boolean isTall) {}
+    record GreetResponse(String greet, List<String> favProgrammingLanguages, Person person) {}
+
 }
